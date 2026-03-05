d9f932b3fc193da857646c4a59bf7279738c0d93
m.hadieva@appdvlprs.com <m.hadieva@appdvlprs.com>
2026-03-06 00:32:58 +0300

    сделала простое решение для сортировки массива

--- wt/main.js
+++ wt/main.js
@@ -1,11 +1,15 @@
+import { Worker } from "worker_threads";
+
 const main = async () => {
-  // Write your code here
-  // Read data.json containing array of numbers
-  // Split into N chunks (N = CPU cores)
-  // Create N workers, send one chunk to each
-  // Collect sorted chunks
-  // Merge using k-way merge algorithm
-  // Log final sorted array
+  const worker = new Worker("./worker.js");
+
+  //worker.postMessage("Ghbdtn");
+
+  worker.postMessage([5, 3, 8, 1, 9, 10, 6, 7]);
+  worker.on("message", (result) => {
+    console.log(result);
+    worker.terminate();
+  });
 };
 
 await main();
--- wt/worker.js
+++ wt/worker.js
@@ -1,9 +1,19 @@
-import { parentPort } from 'worker_threads';
+import { parentPort } from "worker_threads";
 
-// Receive array from main thread
-// Sort in ascending order
-// Send back to main thread
+/**
+ * Получает массив чисел от главного потока,
+ * сортирует их по возрастанию и отправляет обратно
+ */
+parentPort.on("message", (data) => {
+  try {
+    if (!Array.isArray(data)) {
+      throw new Error("Expected array of numbers");
+    }
 
-parentPort.on('message', (data) => {
-  // Write your code here
+    const sorted = data.sort((a, b) => a - b);
+
+    parentPort.postMessage(sorted);
+  } catch (err) {
+    parentPort.postMessage({ error: err.message });
+  }
 });
