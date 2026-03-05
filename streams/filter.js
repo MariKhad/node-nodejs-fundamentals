14b94bfb886cdc09450e2dfbe6cc9c7957881a76
m.hadieva@appdvlprs.com <m.hadieva@appdvlprs.com>
2026-03-05 23:31:22 +0300

    сделала задачи filter и lineNumberer внесла правки

--- streams/filter.js
+++ streams/filter.js
@@ -1,9 +1,42 @@
+import { Transform } from "stream";
+
+/**
+ * Получает паттерн для фильтрации из аргументов командной строки
+ * @returns {string} Паттерн для фильтрацииа
+ */
+const getPattern = () => {
+  let pattern = "";
+
+  const patternIndex = process.argv.indexOf("--pattern");
+
+  if (patternIndex !== -1 && patternIndex + 1 < process.argv.length) {
+    pattern = process.argv[patternIndex + 1];
+  }
+  return pattern;
+};
+
+/**
+ * По заданию функция фильтрует строки вводимые из коммандной строки по определенному паттерну вводимому как аргумент
+ */
 const filter = () => {
-  // Write your code here
-  // Read from process.stdin
-  // Filter lines by --pattern CLI argument
-  // Use Transform Stream
-  // Write to process.stdout
+  const pattern = getPattern();
+
+  if (!pattern) {
+    console.error("Pattern is required");
+    process.exit(1);
+  }
+
+  const transformer = new Transform({
+    transform(chunk, encoding, callback) {
+      const lines = chunk.toString().split("\n");
+      const filtered = lines.filter((line) => line.includes(pattern));
+
+      const result = filtered.join("\n") + (filtered.length > 0 ? "\n" : "");
+
+      callback(null, result);
+    },
+  });
+  process.stdin.pipe(transformer).pipe(process.stdout);
 };
 
 filter();
